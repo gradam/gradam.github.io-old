23013fdee2947829d988ffd616a01109a4f29415
Jakub Semik <kuba.semik@gmail.com>
2020-12-03 08:32:48 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d253310b63bb51f9a73b.js"
+    "url": "webpack-runtime-cacc1c4aeff1d727cf37.js"
   },
   {
     "url": "framework-61416065c0fba8268899.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "87b68092193ca0e853eca0056a11fa1f"
+    "revision": "537a940bd35af2530be67342147257e4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
